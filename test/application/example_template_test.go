0288f27b9036d57b8f865c7391d9197cdac18a7f
Joao Filipe Moura <joao@schub.cloud>
2023-01-18 10:16:20 -0300

    fix: structure

--- test/application/example_template_test.go
+++ test/application/example_template_test.go
@@ -1,14 +1,4 @@
-//go:build kubeall || helm
-// +build kubeall helm
-
-// **NOTE**: we have build tags to differentiate kubernetes tests from non-kubernetes tests, and further differentiate helm
-// tests. This is done because minikube is heavy and can interfere with docker related tests in terratest. Similarly, helm
-// can overload the minikube system and thus interfere with the other kubernetes tests. Specifically, many of the tests
-// start to fail with `connection refused` errors from `minikube`. To avoid overloading the system, we run the kubernetes
-// tests and helm tests separately from the others. This may not be necessary if you have a sufficiently powerful machine.
-// We recommend at least 4 cores and 16GB of RAM if you want to run all the tests together.
-
-package unit
+package application
 
 import (
 	"path/filepath"
