68870365fbaa9797a2e87a60c3bdac1df998600f
João Filipe Moura <joao@schub.cloud>
2023-02-03 09:18:47 -0300

    test/first test (#17)
    * chore: add golden files for test cases
    
    * test: create a base parametrized test case for all templating tests
    
    * chore: track new libraries

---
+++ test/application/template_test.go
@@ -0,0 +1,60 @@
+package application
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gotest.tools/v3/golden"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+)
+
+func TestTemplateRender(t *testing.T) {
+	t.Parallel()
+
+	helmChartPath, err := filepath.Abs("../../application")
+	releaseName := "application"
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name            string
+		values          map[string]string
+		golden          string
+		renderTemplates []string
+	}{
+		{
+			name: "priorityClassLow",
+			values: map[string]string{
+				"priorityClass.enabled": "true",
+				"priorityClass.name":    "low",
+			},
+			golden:          "priorityClass.yaml",
+			renderTemplates: []string{"templates/deployment.yaml"},
+		},
+		{
+			name: "priorityClassHigh",
+			values: map[string]string{
+				"priorityClass.enabled": "true",
+				"priorityClass.name":    "high",
+			},
+			golden:          "priorityClassHigh.yaml",
+			renderTemplates: []string{"templates/deployment.yaml"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(subT *testing.T) {
+			subT.Parallel()
+
+			options := &helm.Options{
+				SetValues: testCase.values,
+			}
+
+			output := helm.RenderTemplate(t, options, helmChartPath, releaseName, testCase.renderTemplates)
+
+			golden.Assert(t, output, testCase.golden)
+		})
+	}
+}
--- test/go.mod
+++ test/go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/googleapis/gnostic v0.4.1 // indirect
@@ -53,6 +54,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools/v3 v3.4.0 // indirect
 	k8s.io/apimachinery v0.20.6 // indirect
 	k8s.io/client-go v0.20.6 // indirect
 	k8s.io/klog/v2 v2.4.0 // indirect
